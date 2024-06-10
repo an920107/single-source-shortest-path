da82bf113e4928c8164d2cd27fa9dbad0f6dd4b0
an920107 <an920107@gmail.com>
2024-06-11 01:44:40 +0800

    refactor: testcases in test.

--- test_main.py
+++ test_main.py
@@ -3,6 +3,37 @@ import unittest
 
 from main import Graph, GraphType
 
+TESTCASES = [
+    [
+        (0, 1, 1),
+        (1, 2, 1),
+        (2, 3, 1),
+        (3, 1, -4),
+        (0, 3, 10),
+    ],
+    [
+        (0, 1, 2),
+        (0, 2, 4),
+        (1, 2, -3),
+        (1, 3, 2),
+        (2, 3, 3),
+    ],
+    [
+        (0, 1, 5),
+        (0, 2, 2),
+        (0, 3, 7),
+        (1, 2, 1),
+        (1, 3, 3),
+        (2, 0, 6),
+    ],
+    [
+        (0, 1, 1),
+        (0, 2, 2),
+        (1, 3, 4),
+        (2, 3, 3),
+    ],
+]
+
 
 class TestGraph(unittest.TestCase):
     def test_init(self):
@@ -28,49 +59,25 @@ class TestGraph(unittest.TestCase):
 
     def test_evaluate_type(self):
         cut = Graph(5)
-        vals = [
-            (0, 1, 1),
-            (1, 2, 1),
-            (2, 3, 1),
-            (3, 1, -4),
-            (0, 3, 10),
-        ]
+        vals = TESTCASES[0]
         for from_vertex, to_vertex, weight in vals:
             cut.add_edge(from_vertex, to_vertex, weight)
         self.assertEqual(cut.evaluate_type(), GraphType.NEG_CYCLE)
 
         cut = Graph(4)
-        vals = [
-            (0, 1, 2),
-            (0, 2, 4),
-            (1, 2, -3),
-            (1, 3, 2),
-            (2, 3, 3),
-        ]
+        vals = TESTCASES[1]
         for from_vertex, to_vertex, weight in vals:
             cut.add_edge(from_vertex, to_vertex, weight)
         self.assertEqual(cut.evaluate_type(), GraphType.NEG_EDGE)
 
         cut = Graph(4)
-        vals = [
-            (0, 1, 5),
-            (0, 2, 2),
-            (0, 3, 7),
-            (1, 2, 1),
-            (1, 3, 3),
-            (2, 0, 6),
-        ]
+        vals = TESTCASES[2]
         for from_vertex, to_vertex, weight in vals:
             cut.add_edge(from_vertex, to_vertex, weight)
         self.assertEqual(cut.evaluate_type(), GraphType.ALL_POS)
 
         cut = Graph(4)
-        vals = [
-            (0, 1, 1),
-            (0, 2, 2),
-            (1, 3, 4),
-            (2, 3, 3),
-        ]
+        vals = TESTCASES[3]
         for from_vertex, to_vertex, weight in vals:
             cut.add_edge(from_vertex, to_vertex, weight)
         self.assertEqual(cut.evaluate_type(), GraphType.DAG)
